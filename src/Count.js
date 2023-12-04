9109a4bf5c167630a81129eb6e4d3d731e5bdf3e
samir berrichi <darksamir2012@gmail.com>
2023-12-04 23:28:35 +0100

    fix

--- src/Count.js
+++ src/Count.js
@@ -1,13 +1,19 @@
 
 import React, { useState } from 'react';
 
+
+// Counter Component
 function Count() {
+
+    // Counter state
     const [counter, setCounter] = useState(0);
 
+    // increase handler
     const increase = () => {
         setCounter(counter + 1);
     };
 
+    //decrease handler
     const decrease = () => {
 
         setCounter(counter - 1);
@@ -20,12 +26,15 @@ function Count() {
             <div className="buttons">
 
                 <div>
+                    {/* decrase button */}
                     <button className='btn-decrease' onClick={() => decrease()}>-</button>
                 </div>
+                {/* check if the counter > 0 or < 0 then change the color  */}
                 <div className={`counter-value ${counter > 0 ? 'counter-value-increase' : ''} ${counter < 0 ? 'counter-value-decrease' : ''}`}>
                     <strong>{counter}</strong>
                 </div>
                 <div>
+                    {/* increase button */}
                     <button className='btn-increase' onClick={() => increase()}>+</button>
                 </div>
 
